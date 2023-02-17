230c755dc2e23998da71c6675cdaf0a1c286407e
ManuKovaSG <kovacicmanu@gmail.com>
2023-02-17 08:51:15 +0100

    luchando con estilos BOOTSTRAP

--- src/pages/Statisticpage.tsx
+++ src/pages/Statisticpage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col, Image } from "react-bootstrap";
+import { Container, Row, Col, Image, Card } from "react-bootstrap";
 import favoritechart from "../Assets/favoritechart.png";
 import vector from "../Assets/Vector.png";
 import trendup from "../Assets/trendup.png";
@@ -29,46 +29,70 @@ const Statisticpage = (props: Props) => {
         }
         `}
       </style>
-        <Container>
-          <div className="boxes">
-      <Row className="justify-content-md-center">
-        <Col>
-          <Image src={favoritechart} />
-        </Col>
-        <Col>
-          {" "}
-          <h2>Recetas más votadas</h2>
-          <p>Sigue nuestra sencilla receta para preparar esta rica paella, ¡incluso si nunca habías preparado una!</p>
-        </Col>
-      </Row>
-      </div>
+      <Container>
+        <div className="boxes">
+          <Row>
+            <Col width="fit-content">
+              <Image src={favoritechart} className="align-items-center" />
+            </Col>
+            <Col fluid>
+              <h2>Recetas más votadas</h2>
+              <p>
+                Sigue nuestra sencilla receta para preparar esta rica paella,
+                ¡incluso si nunca habías preparado una!
+              </p>
+            </Col>
+          </Row>
+        </div>
 
         <div className="boxes">
-      <Row>
-        <Col>
-          <Image src={vector} />
-        </Col>
-        <Col>
-          {" "}
-          <h2>Chefs en tendencia</h2>
-          <p>Sigue nuestra sencilla receta para preparar esta rica paella, ¡incluso si nunca habías preparado una!</p>
-        </Col>
-      </Row>
-      </div>
+          <Row>
+            <Col>
+              <Image src={vector} />
+            </Col>
+            <Col>
+              {" "}
+              <h2>Chefs en tendencia</h2>
+              <p>
+                Sigue nuestra sencilla receta para preparar esta rica paella,
+                ¡incluso si nunca habías preparado una!
+              </p>
+            </Col>
+          </Row>
+        </div>
 
-      <div className="boxes">
-      <Row>
-        <Col>
-          <Image src={trendup} />
-        </Col>
-        <Col>
-          {" "}
-          <h2>Tus estadísticas</h2>
-          <p>Sigue nuestra sencilla receta para preparar esta rica paella, ¡incluso si nunca habías preparado una!</p>
-        </Col>
-      </Row>
-      </div>
-
+        <div className="boxes">
+          <Row>
+            <Col>
+              <Image src={trendup} />
+            </Col>
+            <Col>
+              {" "}
+              <h2>Tus estadísticas</h2>
+              <p className="text-justify">
+                Sigue nuestra sencilla receta para preparar esta rica paella,
+                ¡incluso si nunca habías preparado una!
+              </p>
+            </Col>
+          </Row>
+        </div>
+<Container>
+        <Card style={{ color: "white", backgroundColor: "#171E2B" }}>
+          <Row>
+            <Col>
+              <Card.Img src={trendup} className='' />
+            </Col>
+            <Col>
+              <Card.Body>
+                <Card.Title>Recetas mas votadas</Card.Title>
+                <Card.Text>
+                  Sigue nuestra sencilla receta para preparar esta rica paella,
+                  ¡incluso si nunca habías preparado una!
+                </Card.Text>
+              </Card.Body>
+            </Col>
+          </Row>
+        </Card></Container>
       </Container>
     </>
   );
