d572bf708662445b2d71eacd892ee6376dff922a
ManuKovaSG <kovacicmanu@gmail.com>
2023-02-17 13:25:37 +0100

    agregados estilos grales FONT Y BCKG COLOR

--- src/pages/Statisticpage.tsx
+++ src/pages/Statisticpage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container, Row, Col, Image, Card } from "react-bootstrap";
 import favoritechart from "../Assets/favoritechart.png";
 import vector from "../Assets/Vector.png";
 import trendup from "../Assets/trendup.png";
@@ -8,93 +7,18 @@ type Props = {};
 
 const Statisticpage = (props: Props) => {
   return (
-    <>
-      <style type="text/css">
-        {`
-        .boxes {
-          background-color: #171E2B;
-          margin-bottom: 36px;
-          padding: 1rem;
-          color: white;
-          border-radius: 19px;
-          width: 347px;
-          height: 164px;
-          font-size: 12px;
-        }
-        :root {
-          background-color: black;
-        }
-        .container {
-          background-color: black;
-        }
-        `}
-      </style>
-      <Container>
-        <div className="boxes">
-          <Row>
-            <Col width="fit-content">
-              <Image src={favoritechart} className="align-items-center" />
-            </Col>
-            <Col fluid>
-              <h2>Recetas más votadas</h2>
-              <p>
-                Sigue nuestra sencilla receta para preparar esta rica paella,
-                ¡incluso si nunca habías preparado una!
-              </p>
-            </Col>
-          </Row>
-        </div>
-
-        <div className="boxes">
-          <Row>
-            <Col>
-              <Image src={vector} />
-            </Col>
-            <Col>
-              {" "}
-              <h2>Chefs en tendencia</h2>
-              <p>
-                Sigue nuestra sencilla receta para preparar esta rica paella,
-                ¡incluso si nunca habías preparado una!
-              </p>
-            </Col>
-          </Row>
-        </div>
-
-        <div className="boxes">
-          <Row>
-            <Col>
-              <Image src={trendup} />
-            </Col>
-            <Col>
-              {" "}
-              <h2>Tus estadísticas</h2>
-              <p className="text-justify">
-                Sigue nuestra sencilla receta para preparar esta rica paella,
-                ¡incluso si nunca habías preparado una!
-              </p>
-            </Col>
-          </Row>
-        </div>
-<Container>
-        <Card style={{ color: "white", backgroundColor: "#171E2B" }}>
-          <Row>
-            <Col>
-              <Card.Img src={trendup} className='' />
-            </Col>
-            <Col>
-              <Card.Body>
-                <Card.Title>Recetas mas votadas</Card.Title>
-                <Card.Text>
-                  Sigue nuestra sencilla receta para preparar esta rica paella,
-                  ¡incluso si nunca habías preparado una!
-                </Card.Text>
-              </Card.Body>
-            </Col>
-          </Row>
-        </Card></Container>
-      </Container>
-    </>
+    <div className="max-w-sm mx-auto bg-[#171E2B] rounded-lg overflow-hidden">
+  <div className="sm:flex sm:items-center px-6 py-4">
+    <img className="block mx-auto sm:mx-0 sm:flex-shrink-0 h-16 sm:h-24 rounded-full" src={trendup} alt="trend-up"/>
+    <div className="mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
+      <p className="text-xl leading-tight">Erin Lindford</p>
+      <p className="text-sm leading-tight text-gray-600">Product Engineer</p>
+      <div className="mt-4">
+        <button className="text-purple-500 hover:text-white hover:bg-purple-500 border border-purple-500 text-xs font-semibold rounded-full px-4 py-1 leading-normal">Message</button>
+      </div>
+    </div>
+  </div>
+</div>
   );
 };
 
