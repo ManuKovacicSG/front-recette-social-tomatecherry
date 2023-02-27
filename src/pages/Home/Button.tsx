546ed14a5ea6c57b952eb1eb01bf850dd0a2dc14
albapro <albamaria.idi@gmail.com>
2023-02-27 13:15:20 +0100

    adjustment button

--- src/pages/Home/Button.tsx
+++ src/pages/Home/Button.tsx
@@ -4,11 +4,9 @@ type Props = {}
 
 const Button = (props: Props) => {
   return (
-    <>
-      <button className='bg-[#00C27A] text-black font-extrabold mt-6 text-2xl h-12 w-64 rounded-full'>
+      <a href="/localhost:3000/social" className="bg-[#00C27A] text-center align-middle text-black font-extrabold mt-6 text-2xl h-10 w-64 rounded-full">
         Bon appetit
-      </button>
-    </>
+      </a>
   )
 }
 
