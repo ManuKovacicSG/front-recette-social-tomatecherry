a0b8801fec7938c4c2d465e1cb50486eb9fa34b9
albapro <albamaria.idi@gmail.com>
2023-02-28 09:45:40 +0100

    changes

--- src/pages/Login/Login.test.tsx
+++ src/pages/Login/Login.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from './Login';
 
 describe('Debe haber una pagina para loguearse', () => {
@@ -7,6 +8,24 @@ describe('Debe haber una pagina para loguearse', () => {
         //screen.debug();
         expect(true).toBeTruthy()
     });
+    test('Debe haber un boton para crear una cuenta', ()=>{
+        const button = screen.getByRole('button', {name:'Create account'})
+        expect(button).toBeInTheDocument();
+    })
+    test('Cuando clickeamos en el boton "Create account" nos debe llevar a un formulario para crear un usuario', ()=>{
+        const abrirMockFn = jest.fn();
+        const button = screen.getByRole('button', {name:'Create account'});
+        button.addEventListener('click', abrirMockFn)
+
+        userEvent.click(button);
+
+        expect(abrirMockFn).toBeCalled();
+    })
+
+    test('Debe haber un boton para acceder si ya estoy registrado', ()=>{
+        const button2 = screen.getByRole('button', {name:'Log in'})
+        expect(button2).toBeInTheDocument();
+    })
 
 })
 
