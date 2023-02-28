d4fbcc627e4816da7f4c0e8a68113a5a221e2d4c
ManuKovaSG <kovacicmanu@gmail.com>
2023-03-01 00:36:23 +0100

    appservice arreglado y recette list con hook creado. No funciona aun

--- src/apiServices/appservices.tsx
+++ src/apiServices/appservices.tsx
@@ -2,13 +2,13 @@ import axios from "axios";
 
 const apiBase = 'http://localhost:4000/api/v1'
 
-const taskServices = {
+const appServices = {
     getAllRecipes(){
         return axios.get(apiBase + `/recipes`).then((res) => res.data())
     },
 
-    getRecipeById(id:string){
-        return axios.get(apiBase + `/recipes/${id}`).then((res)=> (res.data));
+    getRecipeById(_id:string){
+        return axios.get(apiBase + `/recipes/${_id}`).then((res)=> (res.data));
     },
 
     deleteRecipeById(id:string){
@@ -17,4 +17,4 @@ const taskServices = {
     }
 }
 
-export default taskServices;
+export default appServices;
--- src/pages/RecetteList/Recette-list.tsx
+++ src/pages/RecetteList/Recette-list.tsx
@@ -1,67 +1,104 @@
-import React from 'react';
-import logo from '../../assets/tomate-logo.svg';
-import cachapa from '../../assets/cachapa.svg'
-import PrimaryNavbar from '../../components/PrimaryNavbar';
+import React, { useEffect, useState } from "react";
+import logo from "../../assets/tomate-logo.svg";
+import cachapa from "../../assets/cachapa.svg";
+import PrimaryNavbar from "../../components/PrimaryNavbar";
+import { useParams } from "react-router-dom";
+import appServices from '../../apiServices/appservices'
 
-type Props = {}
+type Props = {};
 
-const RecetteList = (props: Props) => {
+const RecetteList = (props: Props) => { 
+  let {id} = useParams();
+  let [recipe, setRecipe] = useState({})
+
+  useEffect(()=>{
+    appServices.getRecipeById(id).then((data)=> setRecipe(data));
+  }, [])
+
   return (
-  <body>
-    <section>
-      <article className='p-8 flex flex-cols-3 gap-2 justify-center'>
-        <p className='text-white font-normal'>tomate</p>
-        <img src={logo} alt='logo'></img>
-        <p className='text-white font-normal'>cherry</p>
-      </article>
-      <article className="p-4 container mx-auto px-10 content-center bg-[#00C27A]">
-        <p className='text-white back font-black text-3xl text-center'>What do you want to eat today?</p>
-      </article>
-      <div className='p-6 align-middle'>
-        <input id="" className='m-2' placeholder='Search'></input>
-      </div>
-      <article className='flex flex-cols-3 gap-1 p-4 justify-center'>
-        <button className='bg-[#00C27A] text-white font-extrabold text-sm h-11 w-72 rounded-full'>Recipes top</button>
-        <button className='bg-[#00C27A] text-white font-extrabold text-sm h-11 w-72 rounded-full'>Recommended</button>
-        <button className='bg-[#00C27A] text-white font-extrabold text-sm h-11 w-72 rounded-full'>Breakfast</button>
-      </article>
-      <div className="max-w-sm max-h-44 bg-[#171E2B] rounded-lg overflow-hidden mx-3">
-        <div className="flex flex-cols-3 gap-2 p-2 justify-center">
-          <img className=" m-4 block mx-auto h-24 rounded-full"
-            src={cachapa}
-            alt="trend-up" />
-          <div className=''>
-            <p className="m-2 text-2xl text-white font-bold">Cachapa</p>
-            <p className="m-2 text-sm leading-tight font-light text-white ">Esta receta de cachapas venezolanas caseras con queso son irresistibles, una combinación de sabores dulces y salados</p>
-          </div>
-        </div>
-      </div>
-      <div className="max-w-sm max-h-44 bg-[#171E2B] rounded-lg overflow-hidden mx-3 my-1">
-        <div className="flex flex-cols-3 gap-2 p-2 justify-center">
-          <img className=" m-4 block mx-auto h-24 rounded-full"
-            src={cachapa}
-            alt="trend-up" />
-          <div className=''>
-            <p className="m-2 text-2xl text-white font-bold">Arrollado de papa</p>
-            <p className="m-2 text-sm leading-tight font-light text-white ">Sigue nuestra sencilla receta para preparar esta rica paella, ¡incluso si nunca habías preparado una!</p>
-          </div>
-        </div>
-      </div>
-      <div className="max-w-sm max-h-44 bg-[#171E2B] rounded-lg overflow-hidden mx-3 my-1">
-        <div className="flex flex-cols-3 gap-2 p-2 justify-center">
-          <img className=" m-4 block mx-auto h-24 rounded-full"
-            src={cachapa}
-            alt="trend-up" />
-          <div className=''>
-            <p className="m-2 text-2xl text-white font-bold">Paella</p>
-            <p className="m-2 text-sm leading-tight font-light text-white ">Sigue nuestra sencilla receta para preparar esta rica paella, ¡incluso si nunca habías preparado una!</p>
-          </div>
-        </div>
-       </div> 
-      </section>
-      <PrimaryNavbar/>
-    </body>
-  )
-}
+    <body>
+      <section>
+        <article className="p-8 flex flex-cols-3 gap-2 justify-center">
+          <p className="text-white font-normal">tomate</p>
+          <img src={logo} alt="logo"></img>
+          <p className="text-white font-normal">cherry</p>
+        </article>
 
-export default RecetteList
+        <article className="p-4 container mx-auto px-10 content-center bg-[#00C27A]">
+          <p className="text-white back font-black text-3xl text-center">
+            What do you want to eat today?
+          </p>
+        </article>
+
+        <div className="p-6 align-middle">
+          <input id="" className="m-2" placeholder="Search"></input>
+        </div>
+
+        <article className="flex flex-cols-3 gap-1 p-4 justify-center">
+          <button className="bg-[#00C27A] text-white font-extrabold text-sm h-11 w-72 rounded-full">
+            Recipes top
+          </button>
+          <button className="bg-[#00C27A] text-white font-extrabold text-sm h-11 w-72 rounded-full">
+            Recommended
+          </button>
+          <button className="bg-[#00C27A] text-white font-extrabold text-sm h-11 w-72 rounded-full">
+            Breakfast
+          </button>
+        </article>
+        <div className="max-w-sm max-h-44 bg-[#171E2B] rounded-lg overflow-hidden mx-3">
+          <div className="flex flex-cols-3 gap-2 p-2 justify-center">
+            <img
+              className=" m-4 block mx-auto h-24 rounded-full"
+              src={cachapa}
+              alt="trend-up"
+            />
+            <div className="">
+              <p className="m-2 text-2xl text-white font-bold">Cachapa</p>
+              <p className="m-2 text-sm leading-tight font-light text-white ">
+                Esta receta de cachapas venezolanas caseras con queso son
+                irresistibles, una combinación de sabores dulces y salados
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="max-w-sm max-h-44 bg-[#171E2B] rounded-lg overflow-hidden mx-3 my-1">
+          <div className="flex flex-cols-3 gap-2 p-2 justify-center">
+            <img
+              className=" m-4 block mx-auto h-24 rounded-full"
+              src={cachapa}
+              alt="trend-up"
+            />
+            <div className="">
+              <p className="m-2 text-2xl text-white font-bold">
+                Arrollado de papa
+              </p>
+              <p className="m-2 text-sm leading-tight font-light text-white ">
+                Sigue nuestra sencilla receta para preparar esta rica paella,
+                ¡incluso si nunca habías preparado una!
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="max-w-sm max-h-44 bg-[#171E2B] rounded-lg overflow-hidden mx-3 my-1">
+          <div className="flex flex-cols-3 gap-2 p-2 justify-center">
+            <img
+              className=" m-4 block mx-auto h-24 rounded-full"
+              src={cachapa}
+              alt="trend-up"
+            />
+            <div className="">
+              <p className="m-2 text-2xl text-white font-bold">Paella</p>
+              <p className="m-2 text-sm leading-tight font-light text-white ">
+                Sigue nuestra sencilla receta para preparar esta rica paella,
+                ¡incluso si nunca habías preparado una!
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <PrimaryNavbar />
+    </body>
+  );
+};
+
+export default RecetteList;
