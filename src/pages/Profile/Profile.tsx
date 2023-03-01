10a146f224b122f54d70306ea9b669cd360f8fb3
albapro <albamaria.idi@gmail.com>
2023-03-02 00:48:55 +0100

    add profile navbar

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import PrimaryNavbar from '../../components/PrimaryNavbar'
+import Login from '../Login/Login'
 
 
 const Profile = () => {
   return (
     <>
     <div className='text-white'>This is the profile. Boom!</div>
+    <Login handleSubmit={function (event: React.FormEvent<HTMLFormElement>): void {
+        throw new Error('Function not implemented.')
+      } } />
     <PrimaryNavbar/>
     </>
   )
