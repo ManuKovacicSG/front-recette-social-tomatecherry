2012cd15793ab096a3937e7f67d3cfa24304df8d
ManuKovaSG <kovacicmanu@gmail.com>
2023-03-02 10:02:34 +0100

    cargado formulario de recetas nuevas

--- src/pages/RecipeForm/RecipeForm.tsx
+++ src/pages/RecipeForm/RecipeForm.tsx
@@ -1,15 +1,40 @@
-import React from 'react'
-import PrimaryNavbar from '../../components/PrimaryNavbar'
+import React from "react";
+import PrimaryNavbar from "../../components/PrimaryNavbar";
+import SecondaryNavbar from "../../components/SecondaryNavbar";
 
-type Props = {}
+type Props = {};
 
 const RecipeForm = (props: Props) => {
   return (
     <>
-    <div className='text-white'>RecipeForm</div>
-    <PrimaryNavbar/>
+      <div>
+        <SecondaryNavbar />
+        <form
+          action=""
+          className="md:container md:mx-auto my-8 px-10 grid grid-cols-1 gap-3  "
+        >
+          <label className="text-white">Title</label>
+          <input type="text" placeholder="e.g. Asado de tira" />
+          <label className="text-white">Description</label>
+          <textarea name="description" />
+          <label className="text-white">
+            Cooking time: <input type="number" className="text-black" />
+          </label>
+          <label className="text-white">Ingredients:</label>
+          <select name="ingredients">
+            <option label="choose at least two"></option>
+            <option value="Harina">Harina</option>
+            <option value="Agua">Agua</option>
+            <option value="Sal">Sal</option>
+          </select>
+          <label className="text-white">Tags:</label>
+          <input type="text" />
+        <button className="text-white bg-[#00C27A] p-2 my-7 font-extrabold text-lg h-11 w-72 rounded-full">Submit</button>
+        </form>
+      </div>
+      <PrimaryNavbar />
     </>
-  )
-}
+  );
+};
 
-export default RecipeForm
+export default RecipeForm;
